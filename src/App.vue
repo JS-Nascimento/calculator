8748b9681546563efcdedef27f8e042a4fdea7bf
Jorge Nascimento <sardinha.jorge@gmail.com>
2022-08-14 21:15:43 -0300

    Projetos Vue

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <h1>Calculadora</h1>
+        <CalculatorVue />
+    </div>
+</template>
+
+<script>
+import CalculatorVue from './main/Calculator-Vue.vue'
+export default {
+    components: { CalculatorVue }
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+    font-family: "RobotoMono", monospace;
+}
+
+body {
+    margin: 0;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118 ), rgb(41,46,73));
+}
+</style>
